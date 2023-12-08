512bc8526d9997b323f2866bcd78add323e8cdc3
Ryan Hartman <rhartman1239@gmail.com>
2023-12-07 22:14:43 -0800

    That one wasn't so bad and it made me feel all smart and warm inside

---
+++ d8/solution.py
@@ -0,0 +1,51 @@
+import re
+import math
+
+REGEX = r"(?P<source>[A-Z]+) = \((?P<left>[A-Z]+), (?P<right>[A-Z]+)\)"
+
+
+def parse_line(line: str):
+    match = re.search(REGEX, line)
+    return match["source"], {"L": match["left"], "R": match["right"]}
+
+
+def get_distance(start, mappings, directions):
+    distance = 0
+    source = start
+    while True:
+        for direction in directions:
+            if source.endswith("Z"):
+                return distance
+            distance += 1
+            source = mappings[source][direction]
+
+
+def solution_p1():
+    lines = [l.strip() for l in open("input.txt", "r")]
+    directions = lines[0]
+    mappings = {}
+    for line in lines[2:]:
+        source, destinations = parse_line(line)
+        mappings[source] = destinations
+
+    return get_distance("AAA", mappings, directions)
+
+
+def solution_p2():
+    lines = [l.strip() for l in open("input.txt", "r")]
+    directions = lines[0]
+    mappings = {}
+    sources = []
+    for line in lines[2:]:
+        source, destinations = parse_line(line)
+        mappings[source] = destinations
+        if source.endswith("A"):
+            sources.append(source)
+
+    distances = [get_distance(source, mappings, directions) for source in sources]
+    return math.lcm(*distances)
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {solution_p1()}")
+    print(f"Part 2: {solution_p2()}")
